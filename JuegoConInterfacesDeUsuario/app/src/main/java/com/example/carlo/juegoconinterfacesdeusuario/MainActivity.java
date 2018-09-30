90f6bbd6313fc8bfa4d5b8891d580e307e8635d6
colmejano <carlosgarciaher_@hotmail.com>
2018-09-30 21:59:54 +0200

    Arreglo de bug en el conteo de puntos
    Había un retardo en la corrección de puntos.
    Se ha cambiado la duracion de la aparición del Toast.
    Arreglo del activity de puntos

--- JuegoConInterfacesDeUsuario/app/src/main/java/com/example/carlo/juegoconinterfacesdeusuario/MainActivity.java
+++ JuegoConInterfacesDeUsuario/app/src/main/java/com/example/carlo/juegoconinterfacesdeusuario/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Cargar las preguntas
         lista_preguntas[0] = new PreguntaQuiz("¿Cúal es el nombre de la mascota de Mickey Mouse?","Goofy", "Donald", "Pluto", 3);
-        lista_preguntas[1] = new PreguntaQuiz("¿Cómo se le llama a un grupo de lobos", "Rebaño", "Camada", "Manada", 2);
+        lista_preguntas[1] = new PreguntaQuiz("¿Cómo se le llama a un grupo de lobos", "Rebaño", "Camada", "Manada", 3);
         lista_preguntas[2] = new PreguntaQuiz("¿Qué personajes de un cuento de hadas terminaron en una casa de jengibre?", "Jack y Jill", "Hansel y Gretel", "Alicia y el conejo", 2);
         lista_preguntas[3] = new PreguntaQuiz("¿Qué famoso actor de Hollywood dijo la siguiente frase?: Volveré", "Mel Gibson", "Bruce Willis", "Arnold Schwarzenegger", 3);
         lista_preguntas[4] = new PreguntaQuiz("¿Cuál es el río más largo del mundo?", "Amazona", "Nilo", "Mississippi", 1);
@@ -47,27 +47,27 @@ public class MainActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 if(rpst1.isChecked()==true) {
-                    if(lista_preguntas[0].getSolucion()==1){
-                        MostrarToast("Respuesta correcta");
+                    if(lista_preguntas[contadorPreguntas-1].getSolucion()==1){
                         puntos++;
+                        MostrarToast("Respuesta correcta");
                     }else{
                         MostrarToast("Respuesta incorrecta");
                     }
                     pasarDePregunta(cntP, prgnt, rpst1, rpst2, rpst3);
 
                 }else if(rpst2.isChecked()==true){
-                    if(lista_preguntas[0].getSolucion()==2){
-                        MostrarToast("Respuesta correcta");
+                    if(lista_preguntas[contadorPreguntas-1].getSolucion()==2){
                         puntos++;
+                        MostrarToast("Respuesta correcta");
                     }else{
                         MostrarToast("Respuesta incorrecta");
                     }
                     pasarDePregunta(cntP, prgnt, rpst1, rpst2, rpst3);
 
                 }else if(rpst3.isChecked()==true){
-                    if(lista_preguntas[0].getSolucion()==3){
-                        MostrarToast("Respuesta correcta");
+                    if(lista_preguntas[contadorPreguntas-1].getSolucion()==3){
                         puntos++;
+                        MostrarToast("Respuesta correcta");
                     }else{
                         MostrarToast("Respuesta incorrecta");
                     }
@@ -96,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
             rpst2.setText(lista_preguntas[contadorPreguntas].getRespuesta2());
             rpst3.setText(lista_preguntas[contadorPreguntas].getRespuesta3());
             texto = "Pregunta "+(contadorPreguntas+1);
+            contadorPreguntas++;
+            puntos=0;
             cntP.setText(texto);
         }else {
             prgnt.setText(lista_preguntas[contadorPreguntas].getPregunta());
@@ -108,6 +110,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void MostrarToast(String mag){
-        Toast.makeText(getBaseContext(), mag, Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), mag, Toast.LENGTH_SHORT).show();
     }
 }
